0da2048028b0bdc24f687eb73040727bd9750020
naomi <kiwijang5473@gmail.com>
2021-09-04 13:27:03 +0800

    step3

--- src/app/posts/post.component.ts
+++ src/app/posts/post.component.ts
@@ -5,7 +5,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post.component.scss']
 })
 export class PostComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit(): void {
--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,14 +1,29 @@
+import { Observable } from 'rxjs';
+import { map, shareReplay, switchMap } from 'rxjs/operators';
 import { Component, OnInit } from '@angular/core';
+import { PostService } from '../post.service';
+import { Articles } from '../interfaces/articles';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.scss']
 })
 export class PostsComponent implements OnInit {
+  displayArticles: boolean;
 
-  constructor() { }
+  articles$: Observable<Articles>;
+
+  constructor(private postService: PostService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.articles$ = this.postService.getArticles();
+    this.route.paramMap     //TODO: snapshot 差別
+    .pipe(
+      map(paramMap => paramMap.get('id')),
+      switchMap(id => this.postService.getArticle(id)),
+      shareReplay(1) //TODO: 待補 chach
+    )
   }
 
 }
