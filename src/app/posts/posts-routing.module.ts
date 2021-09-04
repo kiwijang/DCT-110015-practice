6d71d079c182232f31ae5cb3aca0c8d0db02bffa
naomi <kiwijang5473@gmail.com>
2021-09-04 23:41:05 +0800

    Step8 Rout Guard

--- src/app/posts/posts-routing.module.ts
+++ src/app/posts/posts-routing.module.ts
@@ -3,11 +3,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PostComponent } from './post/post.component';
 import { PostsComponent } from './posts.component';
+import { AuthGuard } from '../auth.guard';
 
 const routes: Routes = [
   { path: 'posts', component: PostsComponent },
   { path: 'post/:id', component: PostComponent },
-  { path: 'create', component: CreateComponent },
+  {
+    path: 'create',
+    canActivate: [AuthGuard],
+    component: CreateComponent
+  },
   { path: '', redirectTo: 'posts', pathMatch: 'full' }, // TODO: pathMatch: 'full'???
 ];
 
